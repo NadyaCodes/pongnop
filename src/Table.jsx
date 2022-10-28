464d7ddf2859d53f4ff35ecf09608e51295f7879
NadyaCodes <nadyacorscadden@gmail.com>
2022-10-28 10:17:45 -0600

    add ability to move paddle up and down

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,10 +1,31 @@
 import Paddle from './Paddle'
+import { useState } from 'react'
 
 export default function Table() {
+  const [position, setPosition] = useState('0')
+
+  document.addEventListener('keydown', function(e) {
+    let num = Number(position)
+    switch (e.code) {
+        case "ArrowUp":
+          num--
+          setPosition(num.toString())
+          //  alert('up');
+            break;
+        case "ArrowDown":
+          num++
+          setPosition(num.toString())
+            // alert('down');
+            break;
+        default:
+          break
+    }
+});
+
   return(
     <div className='table'>
       <Paddle side='left' distance='0' />
-      <Paddle side='right' distance='20'/>
+      <Paddle side='right' distance={position}/>
     </div>
   )
 }
